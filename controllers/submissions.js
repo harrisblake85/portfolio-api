a1e68b255ecd24d1b9a3ed6f28bbe92e36e14781
blakeharris <harrisblake85@gmail.com>
2018-01-22 14:04:58 -0700

    added more submissions routes

--- controllers/submissions.js
+++ controllers/submissions.js
@@ -3,13 +3,35 @@ const router = express.Router();
 
 const Submission = require('../models/submissions.js');
 
-router.get("/", async (req,res) => {
-  const submissions = await Submission.find();
-  res.status(200).json({submissions})
+router.post("/", async (req,res) => {
+  const submission = await Submission.create(req.body);
+  res.status(200).json(submission)
 });
 
+router.get("/best", async (req,res) => {
+  const submission = await Submission.findOne({},{},{sort:{likes:-1},limit:1});
+  res.status(200).json(submission)
+});
 
+router.put("/:id", async (req,res) => {
+  const submission = await Submission.findByIdAndUpdate(req.params.id,req.body,{new:true});
+  res.status(200).json(submission)
+});
 
+router.delete("/:id", async (req,res) => {
+  const submission = await Submission.findByIdAndRemove(req.params.id,req.body);
+  res.status(200).json(submission)
+});
+
+router.get("/:id", async (req,res) => {
+  const submissions = await Submission.findById(req.params.id);
+  res.status(200).json(submission)
+});
+
+router.get("/", async (req,res) => {
+  const submissions = await Submission.find();
+  res.status(200).json(submissions)
+});
 
 
 
--- models/submissions.js
+++ models/submissions.js
@@ -4,11 +4,10 @@ const submissionSchema = mongoose.Schema({
   title   : String,
   img     : String,
   desc    : String,
-  likes   : String
-
+  likes   : {type:Number, default:0}
 
 });
 
-// user    : {type: mongoose.Schema.Types.ObjectID, ref:'User'},
-
+//
+// ,user    : {type: mongoose.Schema.Types.ObjectID, ref:'User'}
 module.exports = mongoose.model('Submission', submissionSchema);
