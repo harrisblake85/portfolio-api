79fabfe7d685cc10ed8aa54ead807c84823453b2
blakeharris <harrisblake85@gmail.com>
2018-01-29 04:09:37 -0700

    add root controller

--- server.js
+++ server.js
@@ -63,7 +63,9 @@ const authUser = async (req, res, next) => {
 const submissionsController = require('./controllers/submissions.js');
 
 const usersController = require('./controllers/users.js');
-
+app.controller('/', async (req,res) => {
+  res.send({message:"Hello!"})
+});
 app.use('/users', authUser, usersController);
 // app.use('/sessions', sessionsController);
 
