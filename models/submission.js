511ad30c5262e65dc1ac99781135e10bfc45a2ad
blakeharris <harrisblake85@gmail.com>
2018-01-29 03:06:47 -0700

    Update Submission Model Defaults

--- models/submission.js
+++ models/submission.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 const submissionSchema = mongoose.Schema({
-  title   : String,
-  img     : String,
-  desc    : String,
+  title   : {type:String, default:"A Title Goes Here"},
+  img     : {type:String, default:"https://i.imgur.com/3ssLtPT.png"},
+  desc    : {type:String, default:"A Description Goes Here"},
   likes   : {type:Number, default:0},
   creator : {type: mongoose.Schema.Types.ObjectId, ref:'User'},
   likers  : [{type: mongoose.Schema.Types.ObjectId, ref:'User'}]
@@ -17,7 +17,7 @@ submissionSchema.pre('save', function(next) {
   if (this.isModified('likers')) {
     let temp   = this.likers;
     this.likers = this.likers.filter(onlyUnique);
-    if (this.likers.length< temp.length) {
+    if (this.likers.length < temp.length) {
       this.likes --;
     }
     console.log(this.likers);
