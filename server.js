636edcfb5d6c5fd5786996c348e564786a65a9ae
blakeharris <harrisblake85@gmail.com>
2018-02-09 07:26:36 -0700

    experimentation

--- server.js
+++ server.js
@@ -24,6 +24,31 @@ app.get('/', async (req,res) => {
   res.send({message:"Hello Welcome To Blake Harris!"})
 });
 
+app.get("/email", async (req,res) => {
+  console.log(req);
+  console.log(req.body);
+  const name = req.body.name;
+  const email = req.body.email;
+  const number = req.body.number;
+  const message = req.body.message;
+  const html = "<b>"+" name: "+name+" email: "+email+" number: "+number+" message:"+message+"</b>";
+  var send = require('gmail-send')({
+    user: process.env.GMAIL_EMAIL,
+    pass: process.env.GMAIL_PASS,
+    to: 'harrisblake85@gmail.com',
+    html:html
+  });
+
+
+  send({
+    subject: 'Someone Messaged Me From My Site',
+  }, function (err, res) {
+    console.log('err:', err, '; res:', res);
+  });
+
+  res.send({message:"Ur Good",html})
+});
+
 app.post("/email", async (req,res) => {
   console.log(req);
   console.log(req.body);
